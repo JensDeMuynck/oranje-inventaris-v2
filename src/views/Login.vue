1f03919d8d0ec4ef6a427e1980e5f86497b044c3
Jens De Muynck <demuynck.jens@gmail.com>
2021-05-11 15:27:07 +0200

    [TASK] Scss grid layout base.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,62 +1,55 @@
 <template>
-  <h1>Login</h1>
-  <router-link to="/">Go to overview</router-link>
-  <div @click="vueRouterTest">test vue router</div>
-  <div class="scss-test" @click="vuexTest">test vuex</div>
+	<div class="login"></div>
 
-  <hr />
+	<h1>Login</h1>
+	<router-link to="/">Go to overview</router-link>
 
-  <form @submit.prevent="Login">
-    <input type="text" placeholder="Email" v-model="email"/>
-    <input type="password" placeholder="Password" v-model="password"/>
-    <input type="submit" value="Log in" />
-  </form>
+	<hr />
 
-  <router-link to="/register">Need an account? Register here.</router-link>
+	<form @submit.prevent="Login">
+		<input type="text" placeholder="Email" v-model="email" />
+		<input type="password" placeholder="Password" v-model="password" />
+		<input type="submit" value="Log in" />
+	</form>
+
+	<router-link to="/register">Need an account? Register here.</router-link>
 </template>
 
 <script>
-import { ref } from "vue";
-import firebase from 'firebase/app';
-import 'firebase/auth';
+	import { ref } from "vue";
+	import firebase from "firebase/app";
+	import "firebase/auth";
 
-export default {
-    setup() {
-        const email = ref("")
-        const password = ref("")
+	export default {
+		setup() {
+			const email = ref("");
+			const password = ref("");
 
-        const Login = () => {
-            console.log('Logging in...');
-            firebase
-                .auth()
-                .signInWithEmailAndPassword(email.value, password.value)
-                .then(data => console.log(data))
-                .catch(err => console.warn(err.message))
-        }
+			const Login = () => {
+				console.log("Logging in...");
+				firebase
+					.auth()
+					.signInWithEmailAndPassword(email.value, password.value)
+					.then((data) => console.log(data))
+					.catch((err) => console.warn(err.message));
+			};
 
-        return {
-            Login,
-            email,
-            password
-        }
-
-    },
-    methods: {
-        vueRouterTest() {
-            console.log("test");
-            setTimeout(() => {
-                this.$router.push("/");
-            }, 2000);
-        },
-        vuexTest() {
-            console.log(this.$store.state.user.user);
-        }
-    }
-};
+			return {
+				Login,
+				email,
+				password,
+			};
+		},
+	};
 </script>
 
 <style lang="scss">
-.scss-test {
-  background: $color-red;
+.login {
+	// margin: 0 $margin;
+	background: red;
+	height: 200px;
+
+	@include columns('margin-left', 2);
+	@include columns('width', 2);
 }
 </style>
